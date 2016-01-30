da4bd8387df54923cb3037ed9910d90ba3c7121b
Kirill Kornyakov <kirill.kornyakov@itseez.com>
2016-01-30 15:18:54 +0300

    First perf test

--- perf/perf_skeleton.cpp
+++ perf/perf_skeleton.cpp
@@ -1,6 +1,31 @@
-#include <gtest/gtest.h>
+// #include <gtest/gtest.h>
 
-TEST(perf_skeleton, 2_plus_2_equals_4)
+#include "opencv2/ts/ts.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace perf;
+using namespace cv;
+using std::tr1::make_tuple;
+using std::tr1::get;
+
+
+#define TYPICAL_MAT_SIZES_ABS  ::perf::szVGA, ::perf::sz720p, ::perf::sz1080p
+#define TYPICAL_MATS_ABS       testing::Combine( , testing::Values( TYPICAL_MAT_TYPES_ABS) )
+
+typedef perf::TestBaseWithParam<Size> Size_Only;
+
+PERF_TEST_P(Size_Only, resize, testing::Values(TYPICAL_MAT_SIZES_ABS))
 {
-    EXPECT_EQ(4, 2+2);
+    Size sz = GetParam();
+    cout << sz << endl;
+
+    Size sz_to(sz.width / 2, sz.height / 2);
+
+    cv::Mat src(sz, CV_8UC1), dst(Size(sz_to), CV_8UC1);
+    declare.in(src, WARMUP_RNG).out(dst);
+
+    TEST_CYCLE() resize(src, dst, sz_to);
+
+    SANITY_CHECK(dst, 1 + 1e-6);
 }
