5efb25c0b0832140f88d1e6b3584c7f930efc49c
dirolorbit <kristina.snigura@gmail.com>
2016-02-01 16:38:40 +0300

    Add BGR2GRAY test for one color images

--- test/test_skeleton.cpp
+++ test/test_skeleton.cpp
@@ -94,3 +94,14 @@ TEST(skeleton, ImageResize_correct)
 	EXPECT_EQ(dst.size().height, S.height);
 	EXPECT_EQ(dst.size().width, S.width);
 }
+
+TEST(skeleton, BGR2GRAY_one_color_image_to_one_color_image)
+{
+	Scalar color(123, 32, 203);
+	Mat src(5, 5, CV_8UC3, color);
+	Mat dst;
+	ConvertColor_BGR2GRAY_BT709(src, dst);
+	Mat expectedDst(5, 5, CV_8UC1, dst.at<uchar>(0, 0));
+	Mat match = abs(expectedDst - dst);
+	EXPECT_EQ(0, countNonZero(match));
+}
