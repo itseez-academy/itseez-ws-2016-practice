dd16074c80b9202578ca38eb680fb38631e0b439
Kirill Kornyakov <kirill.kornyakov@itseez.com>
2016-02-01 21:47:26 +0300

    Add empty perf tests

--- perf/perf_skeleton.cpp
+++ perf/perf_skeleton.cpp
@@ -10,17 +10,28 @@ using namespace cv;
 using std::tr1::make_tuple;
 using std::tr1::get;
 
+//
+// Test for the ConvertColor_BGR2GRAY_BT709 function
+//
 
-#define TYPICAL_MAT_SIZES_ABS  ::perf::szVGA, ::perf::sz720p, ::perf::sz1080p
-#define TYPICAL_MATS_ABS       testing::Combine( , testing::Values( TYPICAL_MAT_TYPES_ABS) )
+// PERF_TEST(skeleton, ConvertColor_BGR2GRAY_BT709)
+// {
+//     Mat input = cv::imread("./bin/testdata/sla.png");
+
+//     // Add code here
+// }
+
+//
+// Test for the ImageResize function
+//
+
+#define MAT_SIZES  ::perf::szVGA, ::perf::sz720p, ::perf::sz1080p
 
 typedef perf::TestBaseWithParam<Size> Size_Only;
 
-PERF_TEST_P(Size_Only, ImageResize, testing::Values(TYPICAL_MAT_SIZES_ABS))
+PERF_TEST_P(Size_Only, ImageResize, testing::Values(MAT_SIZES))
 {
     Size sz = GetParam();
-    // cout << sz << endl;
-
     Size sz_to(sz.width / 2, sz.height / 2);
 
     cv::Mat src(sz, CV_8UC1), dst(Size(sz_to), CV_8UC1);
@@ -28,9 +39,25 @@ PERF_TEST_P(Size_Only, ImageResize, testing::Values(TYPICAL_MAT_SIZES_ABS))
 
     TEST_CYCLE()
     {
-        // resize(src, dst, sz_to);
         ImageResize(src, dst, sz_to);
     }
 
     SANITY_CHECK(dst, 1 + 1e-6);
 }
+
+//
+// Test for the skeletonize function
+//
+
+// #define IMAGES testing::Values( std::string("./bin/testdata/sla.png"),\
+//                                 std::string("./bin/testdata/page.png"),\
+//                                 std::string("./bin/testdata/schedule.png") )
+
+// typedef perf::TestBaseWithParam<std::string> ImageName;
+
+// PERF_TEST_P(ImageName, skeletonize, IMAGES)
+// {
+//     Mat input = cv::imread(GetParam());
+
+//     // Add code here
+// }
