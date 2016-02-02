2e15fb316186d9910d523823a2034442b29940e4
UvarovDenis <uvarov.denis@gmail.com>
2016-02-02 18:13:03 +0300

    Add tests for ConvColor and Skeletonize

--- perf/perf_skeleton.cpp
+++ perf/perf_skeleton.cpp
@@ -1,7 +1,6 @@
 #include "opencv_ptest/include/opencv2/ts/ts.hpp"
-
 #include <iostream>
-
+#include <opencv2/highgui/highgui.hpp>
 #include "skeleton_filter.hpp"
 
 using namespace std;
@@ -10,32 +9,31 @@ using namespace cv;
 using std::tr1::make_tuple;
 using std::tr1::get;
 
-
-#define TYPICAL_MAT_SIZES_ABS  ::perf::szVGA, ::perf::sz720p, ::perf::sz1080p
-#define TYPICAL_MATS_ABS       testing::Combine( , testing::Values( TYPICAL_MAT_TYPES_ABS) )
-
-typedef perf::TestBaseWithParam<Size> Size_Only;
-
-// From First Day
-
-//PERF_TEST_P(Size_Only, ImageResize, testing::Values(TYPICAL_MAT_SIZES_ABS))
-//{
-//    Size sz = GetParam();
-//    // cout << sz << endl;
 //
-//    Size sz_to(sz.width / 2, sz.height / 2);
+// Test(s) for the ConvertColor_BGR2GRAY_BT709 function
 //
-//    cv::Mat src(sz, CV_8UC1), dst(Size(sz_to), CV_8UC1);
-//    declare.in(src, WARMUP_RNG).out(dst);
+
+#define IMAGES testing::Values( std::string("./bin/testdata/sla.png"),\
+				std::string("./bin/testdata/page.png"),\
+				std::string("./bin/testdata/schedule.png") )
+
+typedef perf::TestBaseWithParam<std::string> ImageName;
+
+PERF_TEST_P(ImageName, ConvertColor_BGR2GRAY_BT709, IMAGES)
+{
+    Mat input = cv::imread(GetParam());
+    Mat dst(input.size(), CV_8UC1);
+    declare.in(input, WARMUP_RNG).out(dst).iterations(100);
+    TEST_CYCLE()
+    {
+        ConvertColor_BGR2GRAY_BT709(input, dst);
+    }
+    SANITY_CHECK(dst, 1 + 1e-6);
+}
+
 //
-//    TEST_CYCLE()
-//    {
-//        // resize(src, dst, sz_to);
-//        ImageResize(src, dst, sz_to);
-//    }
+// Test(s) for the ImageResize function
 //
-//    SANITY_CHECK(dst, 1 + 1e-6);
-//}
 
 #define MAT_SIZES  ::perf::szVGA, ::perf::sz720p, ::perf::sz1080p
 
@@ -54,5 +52,28 @@ PERF_TEST_P(Size_Only, ImageResize, testing::Values(MAT_SIZES))
         ImageResize(src, dst, sz_to);
     }
 
+    SANITY_CHECK(dst, 1 + 1e-6);
+}
+
+//
+// Test(s) for the skeletonize function
+//
+
+ #define IMAGES testing::Values( std::string("./bin/testdata/sla.png"),\
+                                 std::string("./bin/testdata/page.png"),\
+                                 std::string("./bin/testdata/schedule.png") )
+
+ typedef perf::TestBaseWithParam<std::string> ImageName;
+ 
+ // Without saving
+ PERF_TEST_P(ImageName, skeletonize, IMAGES)
+ {
+     Mat input = cv::imread(GetParam());
+	Mat dst(input.size(), CV_8UC1);
+    declare.in(input, WARMUP_RNG).out(dst).iterations(100).time(30);
+    TEST_CYCLE()
+    {
+        skeletonize(input, dst, false);
+    }
     SANITY_CHECK(dst, 1 + 1e-6);
 }
