9e1e96a48dbbefb6056349eb8efab575cd1e1ee2
Vadim Levin <vadimlevin1@gmail.com>
2016-02-02 16:40:55 +0300

    Added convert color perfomance test

--- perf/perf_skeleton.cpp
+++ perf/perf_skeleton.cpp
@@ -1,7 +1,8 @@
 #include "opencv_ptest/include/opencv2/ts/ts.hpp"
 
 #include <iostream>
-
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
 #include "skeleton_filter.hpp"
 
 using namespace std;
@@ -14,12 +15,26 @@ using std::tr1::get;
 // Test(s) for the ConvertColor_BGR2GRAY_BT709 function
 //
 
-// PERF_TEST(skeleton, ConvertColor_BGR2GRAY_BT709)
-// {
-//     Mat input = cv::imread("./bin/testdata/sla.png");
-//
-//     // Add code here
-// }
+#define IMAGES testing::Values( std::string("./bin/testdata/sla.png"),\
+    std::string("./bin/testdata/page.png"),\
+    std::string("./bin/testdata/schedule.png") )
+
+typedef perf::TestBaseWithParam<std::string> ImageName;
+
+PERF_TEST_P(ImageName, ConvertColor_BGR2GRAY_BT709, IMAGES)
+{
+    //Get parameters
+    Mat input = imread(GetParam());
+    Mat dst(input.size(), CV_8UC1);
+    //declaring input, output and how many iterations should we do
+    declare.in(input, WARMUP_RNG).out(dst).iterations(100);
+    TEST_CYCLE()
+    {
+        ConvertColor_BGR2GRAY_BT709(input, dst);
+    }
+    //Regression check
+    SANITY_CHECK(dst, 1 + 1e-6);
+}
 
 //
 // Test(s) for the ImageResize function
@@ -34,8 +49,8 @@ PERF_TEST_P(Size_Only, ImageResize, testing::Values(MAT_SIZES))
     Size sz = GetParam();
     Size sz_to(sz.width / 2, sz.height / 2);
 
-    cv::Mat src(sz, CV_8UC1), dst(Size(sz_to), CV_8UC1);
-    declare.in(src, WARMUP_RNG).out(dst);
+    Mat src(sz, CV_8UC1), dst(Size(sz_to), CV_8UC1);
+    declare.in(src, WARMUP_RNG).out(dst).iterations(100);
 
     TEST_CYCLE()
     {
@@ -49,15 +64,10 @@ PERF_TEST_P(Size_Only, ImageResize, testing::Values(MAT_SIZES))
 // Test(s) for the skeletonize function
 //
 
-// #define IMAGES testing::Values( std::string("./bin/testdata/sla.png"),\
-//                                 std::string("./bin/testdata/page.png"),\
-//                                 std::string("./bin/testdata/schedule.png") )
-//
-// typedef perf::TestBaseWithParam<std::string> ImageName;
-//
-// PERF_TEST_P(ImageName, skeletonize, IMAGES)
-// {
-//     Mat input = cv::imread(GetParam());
-//
-//     // Add code here
-// }
+
+//PERF_TEST_P(ImageName, skeletonize, IMAGES)
+//{
+//    Mat input = imread(GetParam());
+
+//    // Add code here
+//}
