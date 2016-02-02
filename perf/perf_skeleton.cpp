26e882a5f0019fddd7867ee55e24aa0d689185cf
Sokolov Mikhail <m.s.sokolov.92@gmail.com>
2016-02-02 17:40:33 +0300

    First  perf tests

--- perf/perf_skeleton.cpp
+++ perf/perf_skeleton.cpp
@@ -10,16 +10,27 @@ using namespace cv;
 using std::tr1::make_tuple;
 using std::tr1::get;
 
+#include <opencv2/highgui/highgui.hpp>
+
 //
 // Test(s) for the ConvertColor_BGR2GRAY_BT709 function
 //
 
-// PERF_TEST(skeleton, ConvertColor_BGR2GRAY_BT709)
-// {
-//     Mat input = cv::imread("./bin/testdata/sla.png");
-//
-//     // Add code here
-// }
+PERF_TEST(skeleton, ConvertColor_BGR2GRAY_BT709)
+{
+	Mat input = cv::imread("./bin/testdata/sla.png");
+
+    Mat result = input;
+	declare.in(input, WARMUP_RNG).out(result);
+    
+	// Add code here
+	TEST_CYCLE()
+	{
+		ConvertColor_BGR2GRAY_BT709(input, result);
+	}
+
+	SANITY_CHECK(result, 1 + 1e-6);
+}
 
 //
 // Test(s) for the ImageResize function
@@ -49,15 +60,26 @@ PERF_TEST_P(Size_Only, ImageResize, testing::Values(MAT_SIZES))
 // Test(s) for the skeletonize function
 //
 
-// #define IMAGES testing::Values( std::string("./bin/testdata/sla.png"),\
-//                                 std::string("./bin/testdata/page.png"),\
-//                                 std::string("./bin/testdata/schedule.png") )
-//
-// typedef perf::TestBaseWithParam<std::string> ImageName;
-//
-// PERF_TEST_P(ImageName, skeletonize, IMAGES)
-// {
-//     Mat input = cv::imread(GetParam());
-//
-//     // Add code here
-// }
+ #define IMAGES testing::Values( std::string("./bin/testdata/sla.png"),\
+                                 std::string("./bin/testdata/page.png"),\
+                                 std::string("./bin/testdata/schedule.png") )
+
+ typedef perf::TestBaseWithParam<std::string> ImageName;
+
+PERF_TEST_P(ImageName, skeletonize, IMAGES)
+{
+    Mat input = cv::imread(GetParam());
+
+	Mat result = input;
+	declare.in(input, WARMUP_RNG).out(result);
+	declare.time(30);
+
+    // Add code here
+	TEST_CYCLE()
+    {
+        skeletonize(input, result, false);
+    }
+
+    SANITY_CHECK(result, 1 + 1e-6);
+}
+
--- src/skeleton_filter.cpp
+++ src/skeleton_filter.cpp
@@ -7,33 +7,43 @@ void skeletonize(const cv::Mat &input, cv::Mat &output, bool save_images)
 {
     TS(total);
 
-    TS(imwrite_0);
+    //TS(imwrite_0);
     if (save_images) cv::imwrite("0-input.png", input);
-    TE(imwrite_0);
-
+    //TE(imwrite_0);
+	
     // Convert to grayscale
+	//TS(ConvertColor_BGR2GRAY_BT709);
     cv::Mat gray_image;
     ConvertColor_BGR2GRAY_BT709(input, gray_image);
-    if (save_images) cv::imwrite("1-convertcolor.png", gray_image);
+	//TE(ConvertColor_BGR2GRAY_BT709);
+    if (save_images) { TS(1convertcolor_png); cv::imwrite("1-convertcolor.png", gray_image); TE(1convertcolor_png); }
 
     // Downscale input image
+	//TS(ImageResize);
     cv::Mat small_image;
     cv::Size small_size(input.cols / 1.5, input.rows / 1.5);
     ImageResize(gray_image, small_image, small_size);
-    if (save_images) cv::imwrite("2-resize.png", small_image);
+	//TE(ImageResize);
+    if (save_images) { TS(2resize_png); cv::imwrite("2-resize.png", small_image); TE(2resize_png); }
 
     // Binarization and inversion
+	//TS(threshold);
     cv::threshold(small_image, small_image, 128, 255, cv::THRESH_BINARY_INV);
-    if (save_images) cv::imwrite("3-threshold.png", small_image);
+	//TE(threshold);
+    if (save_images) { TS(3threshold_png); cv::imwrite("3-threshold.png", small_image); TE(3threshold_png); }
 
     // Thinning
+	//TS(GuoHallThinning);
     cv::Mat thinned_image;
     GuoHallThinning(small_image, thinned_image);
-    if (save_images) cv::imwrite("4-thinning.png", thinned_image);
+	//TE(GuoHallThinning);
+	if (save_images) { TS(4thinning_png); cv::imwrite("4-thinning.png", thinned_image); TE(4thinning_png); }
 
+	//TS(imwrite_5);
     // Back inversion
     output = 255 - thinned_image;
     if (save_images) cv::imwrite("5-output.png", output);
+	//TE(imwrite_5);
 
     TE(total);
 }
