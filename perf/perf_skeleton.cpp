295492706a57d93d871871a7826623d0e8856e51
temp110 <temp110@ccam.cmc.unn.net>
2016-02-02 17:54:03 +0300

    add skeletons test

--- perf/perf_skeleton.cpp
+++ perf/perf_skeleton.cpp
@@ -1,8 +1,9 @@
 #include "opencv_ptest/include/opencv2/ts/ts.hpp"
 
 #include <iostream>
-
 #include "skeleton_filter.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
 
 using namespace std;
 using namespace perf;
@@ -10,23 +11,12 @@ using namespace cv;
 using std::tr1::make_tuple;
 using std::tr1::get;
 
-//
-// Test(s) for the ConvertColor_BGR2GRAY_BT709 function
-//
-
-// PERF_TEST(skeleton, ConvertColor_BGR2GRAY_BT709)
-// {
-//     Mat input = cv::imread("./bin/testdata/sla.png");
-//
-//     // Add code here
-// }
-
-//
-// Test(s) for the ImageResize function
-//
-
 #define MAT_SIZES  ::perf::szVGA, ::perf::sz720p, ::perf::sz1080p
+#define IMAGES testing::Values( std::string("testdata\\sla.png"),\
+                                std::string("testdata\\page.png"),\
+                                std::string("testdata\\schedule.png") )
 
+typedef perf::TestBaseWithParam<std::string> ImageName;
 typedef perf::TestBaseWithParam<Size> Size_Only;
 
 PERF_TEST_P(Size_Only, ImageResize, testing::Values(MAT_SIZES))
@@ -34,8 +24,8 @@ PERF_TEST_P(Size_Only, ImageResize, testing::Values(MAT_SIZES))
     Size sz = GetParam();
     Size sz_to(sz.width / 2, sz.height / 2);
 
-    cv::Mat src(sz, CV_8UC1), dst(Size(sz_to), CV_8UC1);
-    declare.in(src, WARMUP_RNG).out(dst);
+    Mat src(sz, CV_8UC1), dst(Size(sz_to), CV_8UC1);
+    declare.in(src, WARMUP_RNG).out(dst).iterations(100).time(30);
 
     TEST_CYCLE()
     {
@@ -45,19 +35,41 @@ PERF_TEST_P(Size_Only, ImageResize, testing::Values(MAT_SIZES))
     SANITY_CHECK(dst, 1 + 1e-6);
 }
 
-//
-// Test(s) for the skeletonize function
-//
+PERF_TEST_P(ImageName, ConvertColor_BGR2GRAY_BT709, IMAGES)
+{
+     Mat input = imread(GetParam());
+	 Mat dst(input.size(),CV_8SC1);
+	 declare.in(input, WARMUP_RNG).out(dst).time(30);
 
-// #define IMAGES testing::Values( std::string("./bin/testdata/sla.png"),\
-//                                 std::string("./bin/testdata/page.png"),\
-//                                 std::string("./bin/testdata/schedule.png") )
-//
-// typedef perf::TestBaseWithParam<std::string> ImageName;
-//
-// PERF_TEST_P(ImageName, skeletonize, IMAGES)
-// {
-//     Mat input = cv::imread(GetParam());
-//
-//     // Add code here
-// }
+	 TEST_CYCLE()
+	 {
+		 ConvertColor_BGR2GRAY_BT709(input,dst);
+	 }
+
+	 SANITY_CHECK(dst, 1 + 1e-6);
+}
+
+PERF_TEST_P(ImageName, skeleton_save, IMAGES)
+{
+    Mat input = imread(GetParam());
+	Mat dst(input.size(), CV_8UC1);
+	declare.in(input, WARMUP_RNG).out(dst).time(20);
+	TEST_CYCLE()
+	{
+		skeletonize(input,dst,false);
+	}
+	SANITY_CHECK(dst, 1 + 1e-6);
+}
+
+PERF_TEST_P(ImageName, skeleton_not_save,IMAGES)
+{
+    Mat input = imread(GetParam());
+	Mat dst(input.size(), CV_8UC1);
+	declare.in(input, WARMUP_RNG).out(dst).time(20);
+	TEST_CYCLE()
+	{
+		skeletonize(input,dst,true);
+	}
+	SANITY_CHECK(dst, 1 + 1e-6);
+}
+
