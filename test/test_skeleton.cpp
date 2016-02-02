9096b02b911ae9a44cdb3534d085017b883e26b6
Pavel Sukhov <pasukhov@yandex-team.ru>
2016-02-02 14:48:08 +0300

    tests

--- test/test_skeleton.cpp
+++ test/test_skeleton.cpp
@@ -4,6 +4,7 @@
 #include "opencv2/imgproc/imgproc.hpp"
 
 #include <iostream>
+#include <iterator>
 
 using namespace cv;
 
@@ -59,4 +60,133 @@ TEST(skeleton, resize_matches_opencv)
     resize(image, reference, sz);
     // std::cout << "Difference:\n" << reference - result << std::endl;
     EXPECT_LT(maxDifference(reference, result), 2);
+}
+
+// Task 1
+TEST(skeleton, test_cvtcolor_dont_resize_image)
+{
+    // Arrange
+    Mat bgr(5, 5, CV_8UC3);
+    randu(bgr, Scalar::all(0), Scalar::all(255));
+
+    // Act
+    Mat result;
+    ConvertColor_BGR2GRAY_BT709(bgr, result);
+
+    // Assert
+    // Looks like overkill...
+    EXPECT_EQ(bgr.dims, result.dims);
+    EXPECT_EQ(bgr.size, result.size);
+}
+
+TEST(skeleton, test_guo_hall_dont_resize_image)
+{
+    // Arrange
+    Mat bgr(5, 5, CV_8UC1); // One color type
+    randu(bgr, Scalar::all(0), Scalar::all(255));
+
+    // Act
+    Mat result;
+    GuoHallThinning(bgr, result);
+
+    // Assert
+    EXPECT_EQ(bgr.dims, result.dims);
+    EXPECT_EQ(bgr.size, result.size);
+}
+
+// Task 2
+TEST(skeleton, test_resize_generates_correctly_sized_image)
+{
+    // Arrange
+    Mat bgr(5, 5, CV_8UC1);
+    randu(bgr, Scalar::all(0), Scalar::all(255));
+    const cv::Size SIZE_TO_SET(8, 13);
+
+    // Act
+    Mat result;
+    ImageResize(bgr, result, SIZE_TO_SET);
+
+    // Assert
+    EXPECT_EQ(SIZE_TO_SET, result.size());
+}
+
+// Task 3
+TEST(skeleton, test_resize_dont_change_color_of_onecolored_image)
+{
+    // Arrange
+    Mat bgr = Mat::ones(20, 20, CV_8UC1);
+
+    const uchar color(142);
+    bgr = color;
+
+    const cv::Size SIZE_TO_SET(8, 13);
+
+    // Act
+    Mat result;
+    ImageResize(bgr, result, SIZE_TO_SET);
+
+    double min, max;
+    Point minLoc, maxLoc;
+
+    minMaxLoc(result, &min, &max, &minLoc, &maxLoc);
+
+    // Assert
+    EXPECT_EQ(color, result.at<uchar>(minLoc.x,minLoc.y));
+    EXPECT_EQ(color, result.at<uchar>(maxLoc.x,minLoc.y));
+}
+
+
+// Task 4
+TEST(skeleton, test_cvtcolor_dont_change_onecolored_image)
+{
+    // Arrange
+    Mat bgr(5, 5, CV_8UC3, CV_RGB(120, 142, 111)); // R G B
+
+    // Act
+    Mat result;
+    ConvertColor_BGR2GRAY_BT709(bgr, result);
+
+    uchar newColor = result.at<uchar>(0, 0);
+
+    // Assert
+
+    double min, max;
+    Point minLoc, maxLoc;
+
+    minMaxLoc(result, &min, &max, &minLoc, &maxLoc);
+
+    // Assert
+    EXPECT_EQ(newColor, result.at<uchar>(minLoc.x, minLoc.y));
+    EXPECT_EQ(newColor, result.at<uchar>(maxLoc.x, minLoc.y));
+}
+
+// Task 5
+TEST(skeleton, test_guo_hall_black_pixel_count)
+{
+    // Arrange
+    Mat bgr(5, 5, CV_8UC1); // One color type
+    randu(bgr, Scalar::all(0), Scalar::all(200));
+
+    // Act
+    Mat result;
+    GuoHallThinning(bgr, result);
+
+    // Assert
+    /*
+    std::cout << "********* Source **********" << std::endl;
+    std::copy(bgr.begin<uchar>(), bgr.end<uchar>(), std::ostream_iterator<uchar>(std::cout, " "));
+    std::cout << std::endl;
+    std::cout << "Black size: " << std::count(bgr.begin<uchar>(), bgr.end<uchar>(), 0) << std::endl;
+    std::cout << "White size: " << std::count(bgr.begin<uchar>(), bgr.end<uchar>(), 255) << std::endl;
+
+    std::cout << "********* Result **********" << std::endl;
+    std::copy(result.begin<uchar>(), result.end<uchar>(), std::ostream_iterator<uchar>(std::cout, " "));
+    std::cout << std::endl;
+    std::cout << "Black size: " << std::count(result.begin<uchar>(), result.end<uchar>(), 0) << std::endl;
+    std::cout << "White size: " << std::count(result.begin<uchar>(), result.end<uchar>(), 255) << std::endl;
+     */
+
+    ASSERT_LE(std::count(bgr.begin<uchar>(), bgr.end<uchar>(), 0),
+              std::count(result.begin<uchar>(), result.end<uchar>(), 0));
+
 }
