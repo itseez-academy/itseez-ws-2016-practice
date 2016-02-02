246afa72da071ea7095734e4da4314a9ae5f2ea1
yanashulgina93 <yana_shulgina93@mail.ru>
2016-02-02 17:44:19 +0300

    TS and TE were used

--- src/skeleton_filter.cpp
+++ src/skeleton_filter.cpp
@@ -13,27 +13,45 @@ void skeletonize(const cv::Mat &input, cv::Mat &output, bool save_images)
 
     // Convert to grayscale
     cv::Mat gray_image;
+	TS(ConvertColor_BGR2GRAY_BT709);
     ConvertColor_BGR2GRAY_BT709(input, gray_image);
+	TE(ConvertColor_BGR2GRAY_BT709);
+	TS(imwrite_1);
     if (save_images) cv::imwrite("1-convertcolor.png", gray_image);
+	TE(imwrite_1);
 
     // Downscale input image
     cv::Mat small_image;
     cv::Size small_size(input.cols / 1.5, input.rows / 1.5);
+	TS(ImageResize);
     ImageResize(gray_image, small_image, small_size);
+	TE(ImageResize);
+	TS(imwrite_2);
     if (save_images) cv::imwrite("2-resize.png", small_image);
+	TE(imwrite_2);
 
     // Binarization and inversion
+	TS(threshold);
     cv::threshold(small_image, small_image, 128, 255, cv::THRESH_BINARY_INV);
+	TE(threshold);
+	TS(imwrite_3);
     if (save_images) cv::imwrite("3-threshold.png", small_image);
+	TE(imwrite_3);
 
     // Thinning
     cv::Mat thinned_image;
+	TS(GuoHallThinning);
     GuoHallThinning(small_image, thinned_image);
+	TE(GuoHallThinning);
+	TS(imwrite_4);
     if (save_images) cv::imwrite("4-thinning.png", thinned_image);
+	TE(imwrite_4);
 
     // Back inversion
     output = 255 - thinned_image;
+	TS(imwrite_5);
     if (save_images) cv::imwrite("5-output.png", output);
+	TE(imwrite_5);
 
     TE(total);
 }
