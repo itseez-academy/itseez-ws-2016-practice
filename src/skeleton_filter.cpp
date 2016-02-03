079766246e97750fcfb1ae627dd56a69b45bf8d6
AnastasiyaPronina <anastasiya.pronina@intel.com>
2016-02-03 08:38:08 +0300

    Logging time measurement in csv file

--- src/skeleton_filter.cpp
+++ src/skeleton_filter.cpp
@@ -2,56 +2,64 @@
 
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
+#include "iostream"
+#include "fstream"
+#include "iomanip"
 
 void skeletonize(const cv::Mat &input, cv::Mat &output, bool save_images)
 {
+	std::ofstream time_logger;
+	time_logger.open("../timelog.csv", std::fstream::out | std::fstream::app);
+
     TS(total);
 
     TS(imwrite_0);
     if (save_images) cv::imwrite("0-input.png", input);
-    TE(imwrite_0);
+    time_logger << TE(imwrite_0);
+
 
     // Convert to grayscale
 	cv::Mat gray_image;
 	TS(convert_color_bgr2gray_bt709);
     ConvertColor_BGR2GRAY_BT709(input, gray_image);
-	TE(convert_color_bgr2gray_bt709);
+	time_logger << TE(convert_color_bgr2gray_bt709);
 	TS(imwrite_1);
     if (save_images) cv::imwrite("1-convertcolor.png", gray_image);
-	TE(imwrite_1);
+	time_logger << TE(imwrite_1);
 	
     // Downscale input image
 	cv::Mat small_image;
     cv::Size small_size(input.cols / 1.5, input.rows / 1.5);
 	TS(imgresize);
     ImageResize(gray_image, small_image, small_size);
-	TE(imgresize);
+	time_logger << TE(imgresize);
 	TS(imwrite_2);
     if (save_images) cv::imwrite("2-resize.png", small_image);
-	TE(imwrite_2);
+	time_logger << TE(imwrite_2);
 	
     // Binarization and inversion
 	TS(threshold);
     cv::threshold(small_image, small_image, 128, 255, cv::THRESH_BINARY_INV);
-	TE(threshold);
+	time_logger << TE(threshold);
 	TS(imwrite_3);
     if (save_images) cv::imwrite("3-threshold.png", small_image);
-	TE(imwrite_3);
+	time_logger << TE(imwrite_3);
 	
     // Thinning
     cv::Mat thinned_image;
 	TS(thinning);
     GuoHallThinning(small_image, thinned_image);
-    TE(thinning);
+    time_logger << TE(thinning);
 	TS(imwrite_4);
 	if (save_images) cv::imwrite("4-thinning.png", thinned_image);
-	TE(imwrite_4);
+	time_logger << TE(imwrite_4);
 	
     // Back inversion
 	output = 255 - thinned_image;
 	TS(imwrite_5);
     if (save_images) cv::imwrite("5-output.png", output);
-	TE(imwrite_5);
+	time_logger << TE(imwrite_5);
 	
-    TE(total);
+	time_logger << TE(total) << std::endl;
+	time_logger.close();
 }
