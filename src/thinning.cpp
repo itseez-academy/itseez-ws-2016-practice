626b245c551d6863cc35b83fea0d9eeb9d70ec88
Vadim Levin <vadimlevin1@gmail.com>
2016-02-03 18:00:23 +0300

    Added first memory optimization (A lot of type casts)

--- src/thinning.cpp
+++ src/thinning.cpp
@@ -1,6 +1,7 @@
 #include "skeleton_filter.hpp"
 #include <opencv2/imgproc/imgproc.hpp>
-
+#include <iostream>
+using namespace std;
 static void GuoHallIteration(cv::Mat& im, int iter)
 {
     cv::Mat marker = cv::Mat::zeros(im.size(), CV_8UC1);
@@ -58,51 +59,118 @@ void GuoHallThinning(const cv::Mat& src, cv::Mat& dst)
 // Place optimized version here
 //
 
-static void GuoHallIteration_optimized(cv::Mat& im, int iter)
+static void Decode(uchar* p, int code)
+{
+        p[0] = (int)(code & 1) > 0 ? 1 : 0;//p2
+        p[1] = (int)(code & 2) > 0 ? 1 : 0;//p3
+        p[2] = (int)(code & 4) >0 ? 1 : 0;//p4
+        p[3] = (int)(code & 8) >0 ? 1 : 0;//p5
+        p[4] = (int)(code & 16) > 0 ? 1 : 0;//p6
+        p[5] = (int)(code & 32) > 0 ? 1 : 0;//p7
+        p[6] = (int)(code & 64) > 0 ? 1 : 0;//p8
+        p[7] = (int)(code & 128) > 0 ? 1 : 0;//p9
+}
+
+static int Encode(uchar* p)
+{
+    char code = p[0] * 1 + p[1] * 2 + p[2] * 4 +
+       p[3] * 8 + p[4] * 16 + p[5] * 32 + p[6] * 64 + p[7] * 128;
+    return static_cast<int>(code);
+}
+
+static void GuoHallIteration_optimized(cv::Mat& im, uchar* neighbours_to_value)
 {
     cv::Mat marker = cv::Mat::zeros(im.size(), CV_8UC1);
-
+    uchar p[8] = {0};
+    uchar code = 0;
     for (int i = 1; i < im.rows-1; i++)
     {
         for (int j = 1; j < im.cols-1; j++)
         {
-            uchar p2 = im.at<uchar>(i-1, j);
-            uchar p3 = im.at<uchar>(i-1, j+1);
-            uchar p4 = im.at<uchar>(i, j+1);
-            uchar p5 = im.at<uchar>(i+1, j+1);
-            uchar p6 = im.at<uchar>(i+1, j);
-            uchar p7 = im.at<uchar>(i+1, j-1);
-            uchar p8 = im.at<uchar>(i, j-1);
-            uchar p9 = im.at<uchar>(i-1, j-1);
-
-            int C  = (!p2 & (p3 | p4)) + (!p4 & (p5 | p6)) +
-                     (!p6 & (p7 | p8)) + (!p8 & (p9 | p2));
-            int N1 = (p9 | p2) + (p3 | p4) + (p5 | p6) + (p7 | p8);
-            int N2 = (p2 | p3) + (p4 | p5) + (p6 | p7) + (p8 | p9);
-            int N  = N1 < N2 ? N1 : N2;
-            int m  = iter == 0 ? ((p6 | p7 | !p9) & p8) : ((p2 | p3 | !p5) & p4);
-
-            if (C == 1 && (N >= 2 && N <= 3) & (m == 0))
-                marker.at<uchar>(i,j) = 1;
+            if(im.at<uchar>(i, j) > 0)
+            {
+                p[0] = im.at<uchar>(i-1, j);
+                p[1] = im.at<uchar>(i-1, j+1);
+                p[2] = im.at<uchar>(i, j+1);
+                p[3] = im.at<uchar>(i+1, j+1);
+                p[4] = im.at<uchar>(i+1, j);
+                p[5] = im.at<uchar>(i+1, j-1);
+                p[6] = im.at<uchar>(i, j-1);
+                p[7] = im.at<uchar>(i-1, j-1);
+                code = Encode(p);
+                marker.at<uchar>(i,j) = neighbours_to_value[code];
+            }
         }
     }
 
     im &= ~marker;
 }
 
+static void calculateTables(uchar* neighbours_to_value_zero, uchar* neighbours_to_value_one)
+{
+    uchar p[8];
+    int  C, N1, N2, N, m_zero, m_one;
+    for(int code = 0; code < 256; code++)
+    {
+        Decode(p, code);
+        // cout<<endl;
+        // for(int j =0; j < 8; j++)
+        // {
+        //     cout<<static_cast<int>(p[j]);
+        // }
+        // cout<<endl;
+        C  = (!p[0] & (p[1] | p[2])) + (!p[2] & (p[3] | p[4])) +
+                     (!p[4] & (p[5] | p[6])) + (!p[6] & (p[7] | p[0]));
+        N1 = (p[7] | p[0]) + (p[1] | p[2]) + (p[3] | p[4]) + (p[5] | p[6]);
+        N2 = (p[0] | p[1]) + (p[2] | p[3]) + (p[4] | p[5]) + (p[6] | p[7]);
+        N  = N1 < N2 ? N1 : N2;
+        m_zero  = ((p[4] | p[5] | !p[7]) & p[6]);
+        m_one = ((p[0] | p[1] | !p[3]) & p[2]);
+        if (C == 1 && (N >= 2 && N <= 3) & (m_zero == 0))
+        {
+            neighbours_to_value_zero[code] = 1;
+        }
+        else
+        {
+            neighbours_to_value_zero[code] = 0;
+        }
+        if (C == 1 && (N >= 2 && N <= 3) & (m_one == 0))
+        {
+            neighbours_to_value_one[code] = 1;
+        }
+        else
+        {
+            neighbours_to_value_one[code] = 0;
+        }
+
+    }
+    // cout<<"$$$$$$$$$"<<endl;
+    // for(int i = 0; i < 256; i++)
+    // {
+    //     cout<<neighbours_to_value_one[i]<<"  ";
+    // }
+    // cout<<"$$$$$$$$$"<<endl;
+}
+
 void GuoHallThinning_optimized(const cv::Mat& src, cv::Mat& dst)
 {
     CV_Assert(CV_8UC1 == src.type());
-
     dst = src / 255;
-
     cv::Mat prev = cv::Mat::zeros(src.size(), CV_8UC1);
     cv::Mat diff;
-
+    uchar neighbours_to_value_one[256] = {0};
+    uchar neighbours_to_value_zero[256] = {0};
+    calculateTables(neighbours_to_value_zero, neighbours_to_value_one);
+    // cout<<"======="<<endl;
+    // for(int i = 0; i < 256; i++)
+    // {
+    //     cout<<static_cast<int>(neighbours_to_value_one[i])<<"  ";
+    // }
+    // cout<<"======"<<endl;
     do
     {
-        GuoHallIteration_optimized(dst, 0);
-        GuoHallIteration_optimized(dst, 1);
+        GuoHallIteration_optimized(dst, neighbours_to_value_zero);
+        GuoHallIteration_optimized(dst, neighbours_to_value_one);
         cv::absdiff(dst, prev, diff);
         dst.copyTo(prev);
     }
