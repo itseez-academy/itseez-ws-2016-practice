f29562604c154b2a1981b9b2115049a3d3c9cec0
yanashulgina93 <yana_shulgina93@mail.ru>
2016-02-03 16:44:37 +0300

    first step for zero pixels

--- src/thinning.cpp
+++ src/thinning.cpp
@@ -3,55 +3,58 @@
 
 static void GuoHallIteration(cv::Mat& im, int iter)
 {
-    cv::Mat marker = cv::Mat::zeros(im.size(), CV_8UC1);
+	cv::Mat marker = cv::Mat::zeros(im.size(), CV_8UC1);
 
-    for (int i = 1; i < im.rows-1; i++)
-    {
-        for (int j = 1; j < im.cols-1; j++)
-        {
-            uchar p2 = im.at<uchar>(i-1, j);
-            uchar p3 = im.at<uchar>(i-1, j+1);
-            uchar p4 = im.at<uchar>(i, j+1);
-            uchar p5 = im.at<uchar>(i+1, j+1);
-            uchar p6 = im.at<uchar>(i+1, j);
-            uchar p7 = im.at<uchar>(i+1, j-1);
-            uchar p8 = im.at<uchar>(i, j-1);
-            uchar p9 = im.at<uchar>(i-1, j-1);
+	for (int i = 1; i < im.rows-1; i++)
+	{
+		for (int j = 1; j < im.cols-1; j++)
+		{
+			if (im.at<uchar>(i,j) !=0)
+			{
+				uchar p2 = im.at<uchar>(i-1, j);
+				uchar p3 = im.at<uchar>(i-1, j+1);
+				uchar p4 = im.at<uchar>(i, j+1);
+				uchar p5 = im.at<uchar>(i+1, j+1);
+				uchar p6 = im.at<uchar>(i+1, j);
+				uchar p7 = im.at<uchar>(i+1, j-1);
+				uchar p8 = im.at<uchar>(i, j-1);
+				uchar p9 = im.at<uchar>(i-1, j-1);
 
-            int C  = (!p2 & (p3 | p4)) + (!p4 & (p5 | p6)) +
-                     (!p6 & (p7 | p8)) + (!p8 & (p9 | p2));
-            int N1 = (p9 | p2) + (p3 | p4) + (p5 | p6) + (p7 | p8);
-            int N2 = (p2 | p3) + (p4 | p5) + (p6 | p7) + (p8 | p9);
-            int N  = N1 < N2 ? N1 : N2;
-            int m  = iter == 0 ? ((p6 | p7 | !p9) & p8) : ((p2 | p3 | !p5) & p4);
+				int C  = (!p2 & (p3 | p4)) + (!p4 & (p5 | p6)) +
+					(!p6 & (p7 | p8)) + (!p8 & (p9 | p2));
+				int N1 = (p9 | p2) + (p3 | p4) + (p5 | p6) + (p7 | p8);
+				int N2 = (p2 | p3) + (p4 | p5) + (p6 | p7) + (p8 | p9);
+				int N  = N1 < N2 ? N1 : N2;
+				int m  = iter == 0 ? ((p6 | p7 | !p9) & p8) : ((p2 | p3 | !p5) & p4);
 
-            if (C == 1 && (N >= 2 && N <= 3) & (m == 0))
-                marker.at<uchar>(i,j) = 1;
-        }
-    }
+				if (C == 1 && (N >= 2 && N <= 3) & (m == 0))
+					marker.at<uchar>(i,j) = 1;
+			}
+		}
+	}
 
-    im &= ~marker;
+	im &= ~marker;
 }
 
 void GuoHallThinning(const cv::Mat& src, cv::Mat& dst)
 {
-    CV_Assert(CV_8UC1 == src.type());
+	CV_Assert(CV_8UC1 == src.type());
 
-    dst = src / 255;
+	dst = src / 255;
 
-    cv::Mat prev = cv::Mat::zeros(src.size(), CV_8UC1);
-    cv::Mat diff;
+	cv::Mat prev = cv::Mat::zeros(src.size(), CV_8UC1);
+	cv::Mat diff;
 
-    do
-    {
-        GuoHallIteration(dst, 0);
-        GuoHallIteration(dst, 1);
-        cv::absdiff(dst, prev, diff);
-        dst.copyTo(prev);
-    }
-    while (cv::countNonZero(diff) > 0);
+	do
+	{
+		GuoHallIteration(dst, 0);
+		GuoHallIteration(dst, 1);
+		cv::absdiff(dst, prev, diff);
+		dst.copyTo(prev);
+	}
+	while (cv::countNonZero(diff) > 0);
 
-    dst *= 255;
+	dst *= 255;
 }
 
 //
@@ -60,55 +63,55 @@ void GuoHallThinning(const cv::Mat& src, cv::Mat& dst)
 
 static void GuoHallIteration_optimized(cv::Mat& im, int iter)
 {
-    cv::Mat marker = cv::Mat::zeros(im.size(), CV_8UC1);
+	cv::Mat marker = cv::Mat::zeros(im.size(), CV_8UC1);
 
-    for (int i = 1; i < im.rows-1; i++)
-    {
-        for (int j = 1; j < im.cols-1; j++)
-        {
-            uchar p2 = im.at<uchar>(i-1, j);
-            uchar p3 = im.at<uchar>(i-1, j+1);
-            uchar p4 = im.at<uchar>(i, j+1);
-            uchar p5 = im.at<uchar>(i+1, j+1);
-            uchar p6 = im.at<uchar>(i+1, j);
-            uchar p7 = im.at<uchar>(i+1, j-1);
-            uchar p8 = im.at<uchar>(i, j-1);
-            uchar p9 = im.at<uchar>(i-1, j-1);
+	for (int i = 1; i < im.rows-1; i++)
+	{
+		for (int j = 1; j < im.cols-1; j++)
+		{
+			uchar p2 = im.at<uchar>(i-1, j);
+			uchar p3 = im.at<uchar>(i-1, j+1);
+			uchar p4 = im.at<uchar>(i, j+1);
+			uchar p5 = im.at<uchar>(i+1, j+1);
+			uchar p6 = im.at<uchar>(i+1, j);
+			uchar p7 = im.at<uchar>(i+1, j-1);
+			uchar p8 = im.at<uchar>(i, j-1);
+			uchar p9 = im.at<uchar>(i-1, j-1);
 
-            int C  = (!p2 & (p3 | p4)) + (!p4 & (p5 | p6)) +
-                     (!p6 & (p7 | p8)) + (!p8 & (p9 | p2));
-            int N1 = (p9 | p2) + (p3 | p4) + (p5 | p6) + (p7 | p8);
-            int N2 = (p2 | p3) + (p4 | p5) + (p6 | p7) + (p8 | p9);
-            int N  = N1 < N2 ? N1 : N2;
-            int m  = iter == 0 ? ((p6 | p7 | !p9) & p8) : ((p2 | p3 | !p5) & p4);
+			int C  = (!p2 & (p3 | p4)) + (!p4 & (p5 | p6)) +
+				(!p6 & (p7 | p8)) + (!p8 & (p9 | p2));
+			int N1 = (p9 | p2) + (p3 | p4) + (p5 | p6) + (p7 | p8);
+			int N2 = (p2 | p3) + (p4 | p5) + (p6 | p7) + (p8 | p9);
+			int N  = N1 < N2 ? N1 : N2;
+			int m  = iter == 0 ? ((p6 | p7 | !p9) & p8) : ((p2 | p3 | !p5) & p4);
 
-            if (C == 1 && (N >= 2 && N <= 3) & (m == 0))
-                marker.at<uchar>(i,j) = 1;
-        }
-    }
+			if (C == 1 && (N >= 2 && N <= 3) & (m == 0))
+				marker.at<uchar>(i,j) = 1;
+		}
+	}
 
-    im &= ~marker;
+	im &= ~marker;
 }
 
 void GuoHallThinning_optimized(const cv::Mat& src, cv::Mat& dst)
 {
-    CV_Assert(CV_8UC1 == src.type());
+	CV_Assert(CV_8UC1 == src.type());
 
-    dst = src / 255;
+	dst = src / 255;
 
-    cv::Mat prev = cv::Mat::zeros(src.size(), CV_8UC1);
-    cv::Mat diff;
+	cv::Mat prev = cv::Mat::zeros(src.size(), CV_8UC1);
+	cv::Mat diff;
 
-    do
-    {
-        GuoHallIteration_optimized(dst, 0);
-        GuoHallIteration_optimized(dst, 1);
-        cv::absdiff(dst, prev, diff);
-        dst.copyTo(prev);
-    }
-    while (cv::countNonZero(diff) > 0);
+	do
+	{
+		GuoHallIteration_optimized(dst, 0);
+		GuoHallIteration_optimized(dst, 1);
+		cv::absdiff(dst, prev, diff);
+		dst.copyTo(prev);
+	}
+	while (cv::countNonZero(diff) > 0);
 
-    dst *= 255;
+	dst *= 255;
 }
 
 //
