4609f93f7828879e0094db9bb51d041beb3b211a
Sokolov Mikhail <m.s.sokolov.92@gmail.com>
2016-02-04 16:54:46 +0300

    floor

--- src/resize.cpp
+++ src/resize.cpp
@@ -65,18 +65,17 @@ void ImageResize_optimized(const cv::Mat &src, cv::Mat &dst, const cv::Size sz)
     {
         uchar *ptr_dst = dst.ptr<uchar>(row);
 
+		const float y = row * yscale + y0;
+        const int iy = (int)(y);
+        const int y1 = iy;
+        const int y2 = iy + 1;
+
         for (int col = 0; col < dst_cols; col++)
         {
             const float x = col * xscale + x0;
-			const float y = row * yscale + y0;
-
-            const int ix = (int)floor(x);
-            const int iy = (int)floor(y);
-
+            const int ix = (int)(x);
             const int x1 = ix;
             const int x2 = ix + 1;
-            const int y1 = iy;
-            const int y2 = iy + 1;
 
             const uchar q11 = src.at<uchar>(y1, x1);
             const uchar q12 = src.at<uchar>(y2, x1);
@@ -87,58 +86,7 @@ void ImageResize_optimized(const cv::Mat &src, cv::Mat &dst, const cv::Size sz)
                              ( (x1 == x2) ? (int)(q11 * (y2 - y) + q22 * (y - y1)) :
                                ( (y1 == y2) ? (int)(q11 * (x2 - x) + q22 * (x - x1)) : 
                                  (int)(q11 * (x2 - x) * (y2 - y) + q21 * (x - x1) * (y2 - y) + q12 * (x2 - x) * (y - y1) + q22 * (x - x1) * (y - y1))));
-            ptr_dst[col] = (temp < 0) ? 0 : ((temp > 255) ? 255 : (uchar)temp);
-        }
-    }
-}
-
-void ImageResize_optimized_1(const cv::Mat &src, cv::Mat &dst, const cv::Size sz)
-{
-    CV_Assert(CV_8UC1 == src.type());
-    cv::Size sz_src = src.size();
-    dst.create(sz, src.type());
-
-    int src_rows = src.rows;
-    int src_cols = src.cols;
-
-    int dst_rows = sz.height;
-    int dst_cols = sz.width;
-
-	float xscale = ((float)sz_src.width) / sz.width;
-	float yscale = ((float)sz_src.height) / sz.height;
-	float x0 = .5f * xscale - .5f;
-	float y0 = .5f * yscale - .5f;
-
-    for (int row = 0; row < dst_rows; row++)
-    {
-        uchar *ptr_dst = dst.ptr<uchar>(row);
-
-        for (int col = 0; col < dst_cols; col++)
-        {
-            float x = col * xscale + x0;
-			float y = row * yscale + y0;
-
-            int ix = (int)floor(x);
-			int iy = (int)floor(y);
-
-            int x1 = ix;
-            int x2 = ix + 1;
-			int y1 = iy;
-			int y2 = iy + 1;
-
-            uchar q11 = src.at<uchar>(y1, x1);
-            uchar q12 = src.at<uchar>(y2, x1);
-            uchar q21 = src.at<uchar>(y1, x2);
-            uchar q22 = src.at<uchar>(y2, x2);
-
-            int temp = ((x1 == x2) && (y1 == y2)) ? (int)q11 :
-              ( (x1 == x2) ? (int)(q11 * (y2 - y) + q22 * (y - y1)) :
-              ( (y1 == y2) ? (int)(q11 * (x2 - x) + q22 * (x - x1)) : 
-              (int)(q11 * (x2 - x) * (y2 - y) + q21 * (x - x1) * (y2 - y) + q12 * (x2 - x) * (y - y1) + q22 * (x - x1) * (y - y1))));
-
-			//printf("(x,y)=(%f,%f)\t(ix,iy)=(%i,%i)\t(x1,y1)=(%i,%i)\t(x2,y2)=(%i,%i)\n", x, y, ix, iy, x1, y1, x2, y2);
-
-            ptr_dst[col] = (temp < 0) ? 0 : ((temp > 255) ? 255 : (uchar)temp);
+            ptr_dst[col] = (uchar)temp;
         }
     }
 }
