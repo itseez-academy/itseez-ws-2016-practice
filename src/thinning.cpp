658eb76d4eab3933b5d33e4651a9a6864b3978e7
DmitryKurtaev <dmitry.kurtaev@gmail.com>
2016-02-05 00:30:10 +0300

    Skipped zeros

--- src/thinning.cpp
+++ src/thinning.cpp
@@ -66,24 +66,27 @@ static void GuoHallIteration_optimized(cv::Mat& im, int iter)
     {
         for (int j = 1; j < im.cols-1; j++)
         {
-            uchar p2 = im.at<uchar>(i-1, j);
-            uchar p3 = im.at<uchar>(i-1, j+1);
-            uchar p4 = im.at<uchar>(i, j+1);
-            uchar p5 = im.at<uchar>(i+1, j+1);
-            uchar p6 = im.at<uchar>(i+1, j);
-            uchar p7 = im.at<uchar>(i+1, j-1);
-            uchar p8 = im.at<uchar>(i, j-1);
-            uchar p9 = im.at<uchar>(i-1, j-1);
+            if (im.at<uchar>(i, j) != 0)
+            {
+                uchar p2 = im.at<uchar>(i-1, j);
+                uchar p3 = im.at<uchar>(i-1, j+1);
+                uchar p4 = im.at<uchar>(i, j+1);
+                uchar p5 = im.at<uchar>(i+1, j+1);
+                uchar p6 = im.at<uchar>(i+1, j);
+                uchar p7 = im.at<uchar>(i+1, j-1);
+                uchar p8 = im.at<uchar>(i, j-1);
+                uchar p9 = im.at<uchar>(i-1, j-1);
 
-            int C  = (!p2 & (p3 | p4)) + (!p4 & (p5 | p6)) +
-                     (!p6 & (p7 | p8)) + (!p8 & (p9 | p2));
-            int N1 = (p9 | p2) + (p3 | p4) + (p5 | p6) + (p7 | p8);
-            int N2 = (p2 | p3) + (p4 | p5) + (p6 | p7) + (p8 | p9);
-            int N  = N1 < N2 ? N1 : N2;
-            int m  = iter == 0 ? ((p6 | p7 | !p9) & p8) : ((p2 | p3 | !p5) & p4);
+                int C  = (!p2 & (p3 | p4)) + (!p4 & (p5 | p6)) +
+                         (!p6 & (p7 | p8)) + (!p8 & (p9 | p2));
+                int N1 = (p9 | p2) + (p3 | p4) + (p5 | p6) + (p7 | p8);
+                int N2 = (p2 | p3) + (p4 | p5) + (p6 | p7) + (p8 | p9);
+                int N  = N1 < N2 ? N1 : N2;
+                int m  = iter == 0 ? ((p6 | p7 | !p9) & p8) : ((p2 | p3 | !p5) & p4);
 
-            if (C == 1 && (N >= 2 && N <= 3) & (m == 0))
-                marker.at<uchar>(i,j) = 1;
+                if (C == 1 && (N >= 2 && N <= 3) & (m == 0))
+                    marker.at<uchar>(i,j) = 1;
+            }
         }
     }
 
