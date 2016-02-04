e1f74aae71eef40cfe16f12727d46656f8252c90
Maxim Doronin <doro156@me.com>
2016-02-04 18:22:17 +0300

    Move operations that are independent of the iteration

--- src/resize.cpp
+++ src/resize.cpp
@@ -64,19 +64,19 @@ void ImageResize_optimized(const cv::Mat &src, cv::Mat &dst, const cv::Size sz)
     for (int row = 0; row < dst_rows; row++)
     {
         uchar *ptr_dst = dst.ptr<uchar>(row);
+		const float y = (float)row * yscale + y0;
+		const int iy = (int)floor(y);
+		const int y1 = (iy < 0) ? 0 : ((iy >= src_rows) ? src_rows - 1 : iy);
+        const int y2 = (iy < 0) ? 0 : ((iy >= src_rows - 1) ? src_rows - 1 : iy + 1);
 
         for (int col = 0; col < dst_cols; col++)
         {
 			const float x = (float)col * xscale + x0;
-			const float y = (float)row * yscale + y0;
-
+			
             const int ix = (int)floor(x);
-            const int iy = (int)floor(y);
-
+            
             const int x1 = (ix < 0) ? 0 : ((ix >= src_cols) ? src_cols - 1 : ix);
             const int x2 = (ix < 0) ? 0 : ((ix >= src_cols - 1) ? src_cols - 1 : ix + 1);
-            const int y1 = (iy < 0) ? 0 : ((iy >= src_rows) ? src_rows - 1 : iy);
-            const int y2 = (iy < 0) ? 0 : ((iy >= src_rows - 1) ? src_rows - 1 : iy + 1);
 
             const uchar q11 = src.at<uchar>(y1, x1);
             const uchar q12 = src.at<uchar>(y2, x1);
