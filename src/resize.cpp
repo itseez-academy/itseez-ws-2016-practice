98381a7a7dc427a5bca9959271b00bc804c99093
Evgeny Gorbachev <volartm@gmail.com>
2016-02-05 00:43:35 +0300

    Gorbachev. image resize optimization

--- src/resize.cpp
+++ src/resize.cpp
@@ -77,10 +77,10 @@ void ImageResize_optimized(const cv::Mat &src, cv::Mat &dst, const cv::Size sz)
                 const int ix = (int)(x);
                 const int iy = (int)(y);
 
-                const int x1 = (ix < 0) ? 0 : ((ix >= src_cols) ? src_cols - 1 : ix);
-                const int x2 = (ix < 0) ? 0 : ((ix >= src_cols - 1) ? src_cols - 1 : ix + 1);
-                const int y1 = (iy < 0) ? 0 : ((iy >= src_rows) ? src_rows - 1 : iy);
-                const int y2 = (iy < 0) ? 0 : ((iy >= src_rows - 1) ? src_rows - 1 : iy + 1);
+                const int x1 = ( ix);
+                const int x2 = ( ix + 1);
+                const int y1 = ( iy);
+                const int y2 = ( iy + 1);
 
                 const uchar q11 = src.at<uchar>(y1, x1);
                 const uchar q12 = src.at<uchar>(y2, x1);
@@ -106,8 +106,8 @@ void ImageResize_optimized(const cv::Mat &src, cv::Mat &dst, const cv::Size sz)
                 const float x = ((float) col) * xscale + x0;
                 const float y = ((float) row) * yscale + y0;
 
-                const int ix = (int)(x);
-                const int iy = (int)(y);
+                const int ix = (int)floor(x);
+                const int iy = (int)floor(y);
 
                 const int x1 = (ix);
                 const int x2 = (ix + 1);
