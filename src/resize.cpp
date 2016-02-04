2f44ced819a0b4e5a29551c427c2cb7a96c173df
temp110 <temp110@ccam.cmc.unn.net>
2016-02-04 16:26:33 +0300

    Optimal resize 3.45

--- src/resize.cpp
+++ src/resize.cpp
@@ -1,5 +1,6 @@
 #include "skeleton_filter.hpp"
 #include <math.h>
+#include <assert.h>
 
 void ImageResize(const cv::Mat &src, cv::Mat &dst, const cv::Size sz)
 {
@@ -57,31 +58,71 @@ void ImageResize_optimized(const cv::Mat &src, cv::Mat &dst, const cv::Size sz)
     const int dst_rows = sz.height;
     const int dst_cols = sz.width;
 
-    for (int row = 0; row < dst_rows; row++)
-    {
-        uchar *ptr_dst = dst.ptr<uchar>(row);
+	float ratioX = (float)sz_src.width  / sz.width;
+	float ratioY = (float)sz_src.height / sz.height;
+	float offsetX = 0.5f*ratioX - 0.5f;
+	float offsetY = 0.5f*ratioY - 0.5f;
 
-        for (int col = 0; col < dst_cols; col++)
-        {
-            const float x = (((float)col) + .5f) * sz_src.width  / sz.width  - .5f;
-            const float y = (((float)row) + .5f) * sz_src.height / sz.height - .5f;
+	if (src_rows > dst_rows && src_cols > dst_cols)
+	{
+		for (int row = 0; row < dst_rows; row++)
+		{
+			uchar *ptr_dst = dst.ptr<uchar>(row);
+			const float y = row*ratioY + offsetY;
+			const int iy = (y<0) ? (int)floor(y) : (int)y;
+			const int y1 = iy;
+			const int y2 = iy+1;
 
-            const int ix = (int)floor(x);
-            const int iy = (int)floor(y);
+			for (int col = 0; col < dst_cols; col++)
+			{
+				const float x = col*ratioX + offsetX;
+				const int ix = (x<0) ? (int)floor(x) : (int)x;
 
-            const int x1 = (ix < 0) ? 0 : ((ix >= src_cols) ? src_cols - 1 : ix);
-            const int x2 = (ix < 0) ? 0 : ((ix >= src_cols - 1) ? src_cols - 1 : ix + 1);
-            const int y1 = (iy < 0) ? 0 : ((iy >= src_rows) ? src_rows - 1 : iy);
-            const int y2 = (iy < 0) ? 0 : ((iy >= src_rows - 1) ? src_rows - 1 : iy + 1);
+				const int x1 = ix;
+				const int x2 = ix+1;
 
-            const uchar q11 = src.at<uchar>(y1, x1);
-            const uchar q12 = src.at<uchar>(y2, x1);
-            const uchar q21 = src.at<uchar>(y1, x2);
-            const uchar q22 = src.at<uchar>(y2, x2);
+				const int q11 = src.at<uchar>(y1, x1);
+				const int q12 = src.at<uchar>(y2, x1);
+				const int q21 = src.at<uchar>(y1, x2);
+				const int q22 = src.at<uchar>(y2, x2);
 
-            const int temp = (x1 == x2) ? (int)(q11 * (y2 - y) + q22 * (y - y1)) :
-                            ((y1 == y2) ? (int)(q11 * (x2 - x) + q22 * (x - x1)) : (int)(q11 * (x2 - x) * (y2 - y) + q21 * (x - x1) * (y2 - y) + q12 * (x2 - x) * (y - y1) + q22 * (x - x1) * (y - y1)));
-            ptr_dst[col] = (temp < 0) ? 0 : ((temp > 255) ? 255 : (uchar)temp);
-        }
-    }
+				const int temp = ((x1 == x2) && (y1 == y2)) ? q11 :
+								 ( (x1 == x2) ? (q11 * (y2 - y) + q22 * (y - y1)) :
+								   ( (y1 == y2) ? (q11 * (x2 - x) + q22 * (x - x1)) : 
+									 (q11 * (x2 - x) * (y2 - y) + q21 * (x - x1) * (y2 - y) + q12 * (x2 - x) * (y - y1) + q22 * (x - x1) * (y - y1))));
+				ptr_dst[col] = temp;//(temp < 0) ? 0 : ((temp > 255) ? 255 : (uchar)temp);
+			}
+		}
+	}
+	else
+	{
+		for (int row = 0; row < dst_rows; row++)
+		{
+			uchar *ptr_dst = dst.ptr<uchar>(row);
+			const float y = row*ratioY + offsetY;
+			const int iy = (y<0) ? (int)floor(y) : (int)y;
+			const int y1 = (iy < 0) ? 0 : ((iy >= src_rows) ? src_rows - 1 : iy);
+			const int y2 = (iy < 0) ? 0 : ((iy >= src_rows - 1) ? src_rows - 1 : iy + 1);
+
+			for (int col = 0; col < dst_cols; col++)
+			{
+				const float x = col*ratioX + offsetX;
+				const int ix = (x<0) ? (int)floor(x) : (int)x;
+
+				const int x1 = (ix < 0) ? 0 : ((ix >= src_cols) ? src_cols - 1 : ix);
+				const int x2 = (ix < 0) ? 0 : ((ix >= src_cols - 1) ? src_cols - 1 : ix + 1);
+
+				const int q11 = src.at<uchar>(y1, x1);
+				const int q12 = src.at<uchar>(y2, x1);
+				const int q21 = src.at<uchar>(y1, x2);
+				const int q22 = src.at<uchar>(y2, x2);
+
+				const int temp = ((x1 == x2) && (y1 == y2)) ? q11 :
+								 ( (x1 == x2) ? (q11 * (y2 - y) + q22 * (y - y1)) :
+								   ( (y1 == y2) ? (q11 * (x2 - x) + q22 * (x - x1)) : 
+									 (q11 * (x2 - x) * (y2 - y) + q21 * (x - x1) * (y2 - y) + q12 * (x2 - x) * (y - y1) + q22 * (x - x1) * (y - y1))));
+				ptr_dst[col] = temp;//(temp < 0) ? 0 : ((temp > 255) ? 255 : (uchar)temp);
+			}
+		}
+	}
 }
