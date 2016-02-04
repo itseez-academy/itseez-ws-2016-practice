f224c08762fc7c3eb0ca8f7ebf2243fcdb695cd0
Voronkov-A <arseny-voronkov@yandex.ru>
2016-02-04 15:58:34 +0300

    fix conditions

--- src/resize.cpp
+++ src/resize.cpp
@@ -47,47 +47,57 @@ void ImageResize(const cv::Mat &src, cv::Mat &dst, const cv::Size sz)
 void ImageResize_optimized(const cv::Mat &src, cv::Mat &dst, const cv::Size sz)
 {
     CV_Assert(CV_8UC1 == src.type());
-    cv::Size sz_src = src.size();
-    dst.create(sz, src.type());
-
-    const int src_rows = src.rows;
-    const int src_cols = src.cols;
-
-    const int dst_rows = sz.height;
-    const int dst_cols = sz.width;
-
-    const float xScale = (float)src_cols / (float)dst_cols;
-    const float yScale = (float)src_rows / (float)dst_rows;
-    const float x0 = (xScale - 1.0f) * 0.5f;
-    const float y0 = (yScale - 1.0f) * 0.5f;
-
-    for (int row = 0; row < dst_rows; row++)
+    if (!(src.cols == sz.width || src.rows == sz.height))
     {
-        uchar *ptr_dst = dst.ptr<uchar>(row);
+        cv::Size sz_src = src.size();
+        dst.create(sz, src.type());
 
-        for (int col = 0; col < dst_cols; col++)
+        const int src_rows = src.rows;
+        const int src_cols = src.cols;
+
+        const int dst_rows = sz.height;
+        const int dst_cols = sz.width;
+
+        const float xScale = (float)src_cols / (float)dst_cols;
+        const float yScale = (float)src_rows / (float)dst_rows;
+        const float x0 = (xScale - 1.0f) * 0.5f;
+        const float y0 = (yScale - 1.0f) * 0.5f;
+
+        for (int row = 0; row < dst_rows; row++)
         {
-            const float x = col * xScale + x0;
-            const float y = row * yScale + y0;
+            uchar *ptr_dst = dst.ptr<uchar>(row);
 
-            const int ix = (int)x;
-            const int iy = (int)y;
+            for (int col = 0; col < dst_cols; col++)
+            {
+                const float x = col * xScale + x0;
+                const float y = row * yScale + y0;
 
-            const int x1 = (ix < 0) ? 0 : ((ix >= src_cols) ? src_cols - 1 : ix);
-            const int x2 = (ix < 0) ? 0 : ((ix >= src_cols - 1) ? src_cols - 1 : ix + 1);
-            const int y1 = (iy < 0) ? 0 : ((iy >= src_rows) ? src_rows - 1 : iy);
-            const int y2 = (iy < 0) ? 0 : ((iy >= src_rows - 1) ? src_rows - 1 : iy + 1);
+                const int ix = (int)x;
+                const int iy = (int)y;
 
-            const uchar q11 = src.at<uchar>(y1, x1);
-            const uchar q12 = src.at<uchar>(y2, x1);
-            const uchar q21 = src.at<uchar>(y1, x2);
-            const uchar q22 = src.at<uchar>(y2, x2);
+                const int x1 = ix;
+                const int x2 = ix + 1;
+                const int y1 = iy;
+                const int y2 = iy + 1;
 
-            const int temp = ((x1 == x2) && (y1 == y2)) ? (int)q11 :
-              ( (x1 == x2) ? (int)(q11 * (y2 - y) + q22 * (y - y1)) :
-              ( (y1 == y2) ? (int)(q11 * (x2 - x) + q22 * (x - x1)) : 
-              (int)(q11 * (x2 - x) * (y2 - y) + q21 * (x - x1) * (y2 - y) + q12 * (x2 - x) * (y - y1) + q22 * (x - x1) * (y - y1))));
-            ptr_dst[col] = (temp < 0) ? 0 : ((temp > 255) ? 255 : (uchar)temp);
+                const uchar q11 = src.at<uchar>(y1, x1);
+                const uchar q12 = src.at<uchar>(y2, x1);
+                const uchar q21 = src.at<uchar>(y1, x2);
+                const uchar q22 = src.at<uchar>(y2, x2);
+
+                const int temp = ((x1 == x2) && (y1 == y2)) ? (int)q11 :
+                  ( (x1 == x2) ? (int)(q11 * (y2 - y) + q22 * (y - y1)) :
+                  ( (y1 == y2) ? (int)(q11 * (x2 - x) + q22 * (x - x1)) : 
+                  (int)(q11 * (x2 - x) * (y2 - y) + 
+                  q21 * (x - x1) * (y2 - y) + 
+                  q12 * (x2 - x) * (y - y1) + 
+                  q22 * (x - x1) * (y - y1))));
+                ptr_dst[col] = (temp < 0) ? 0 : ((temp > 255) ? 255 : (uchar)temp);
+            }
         }
+    }
+    else
+    {
+        dst = src.clone();
     }
 }
