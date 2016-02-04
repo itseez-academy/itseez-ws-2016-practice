4c76aec08d12dccfc28d5d563a7a6955c947977e
Maxim Doronin <doro156@me.com>
2016-02-04 19:01:26 +0300

    Removed the condition in result

--- src/resize.cpp
+++ src/resize.cpp
@@ -85,7 +85,7 @@ void ImageResize_optimized(const cv::Mat &src, cv::Mat &dst, const cv::Size sz)
 
             const int temp = 
               (int)(q11 * (x2 - x) * (y2 - y) + q21 * (x - x1) * (y2 - y) + q12 * (x2 - x) * (y - y1) + q22 * (x - x1) * (y - y1));
-            ptr_dst[col] = (temp < 0) ? 0 : ((temp > 255) ? 255 : (uchar)temp);
+            ptr_dst[col] = (uchar)temp;
         }
     }
 }
