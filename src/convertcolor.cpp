7aead815f93a20b63b8841ddd141f007f8e5a455
k0tk0v <annn.asmanova@gmail.com>
2016-02-05 16:46:49 +0300

    floatToInt change

--- src/convertcolor.cpp
+++ src/convertcolor.cpp
@@ -67,8 +67,11 @@ void ConvertColor_BGR2GRAY_BT709_fpt(const cv::Mat& src, cv::Mat& dst)
 
         for (int x = 0; x < sz.width; x++)
         {
-            float color = 0.2126 * psrc[x][2-bidx] + 0.7152 * psrc[x][1] + 0.0722 * psrc[x][bidx];
-            pdst[x] = (int)(color + 0.5);
+            //float color = 0.2126 * psrc[x][2-bidx] + 0.7152 * psrc[x][1] + 0.0722 * psrc[x][bidx];
+          //  pdst[x] = (int)(color + 0.5);
+
+			int color = (21260 * psrc[x][2-bidx] + 71520*psrc[x][1] + 7220*psrc[x][bidx] + 50000)/100000;
+			pdst[x]= color;
         }
     }
 }
