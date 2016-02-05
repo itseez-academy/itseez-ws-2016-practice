6d7705a95648dc8dd7c6561ff86fe7ff73f174b5
Evgeny Gorbachev <volartm@gmail.com>
2016-02-05 17:12:10 +0300

    Gorbachev. BGR2GRAY float to int

--- src/convertcolor.cpp
+++ src/convertcolor.cpp
@@ -67,8 +67,8 @@ void ConvertColor_BGR2GRAY_BT709_fpt(const cv::Mat& src, cv::Mat& dst)
 
         for (int x = 0; x < sz.width; x++)
         {
-            float color = 0.2126 * psrc[x][2-bidx] + 0.7152 * psrc[x][1] + 0.0722 * psrc[x][bidx];
-            pdst[x] = (int)(color + 0.5);
+            __int32_t color = 2126 * psrc[x][2-bidx] + 7152 * psrc[x][1] + 722 * psrc[x][bidx];
+            pdst[x] = (int)(static_cast<float>(color)*0.0001 + 0.5);
         }
     }
 }
