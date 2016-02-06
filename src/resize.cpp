37264d3b4262896cd7f35c6c68610600e9cc688a
DmitryKurtaev <dmitry.kurtaev@gmail.com>
2016-02-06 21:24:57 +0300

    Implemented different cases of inteprolation

--- src/resize.cpp
+++ src/resize.cpp
@@ -57,36 +57,54 @@ void ImageResize_optimized(const cv::Mat &src, cv::Mat &dst, const cv::Size sz)
     const float scale_y = (float)(src.rows) / dst_rows;;
     const float scalar_y0 = 0.5f * scale_y  - 0.5f;
 
-    for (int row = 0; row < dst_rows; row++)
+    // (col+0.5)*scale-0.5 < 0
+    int last_negative_row = (int)(0.5f / scale_y - 0.5f);
+    int last_negative_col = (int)(0.5f / scale_x - 0.5f);
+
+    // Negative rows.
+    const uchar* upper_row = src.ptr<uchar>(0);
+    for (int row = 0; row < last_negative_row; ++row)
     {
-        uchar *ptr_dst = dst.ptr<uchar>(row);
-
+        uchar* ptr_dst = dst.ptr<uchar>(row);
         const float y = row * scale_y + scalar_y0;
-        const int iy = (int)floor(y);
-        const int y1 = (iy < 0) ? 0 : iy;
-        const int y2 = iy + 1;
-        const uchar* upper_row = src.ptr<uchar>(y1);
-        const uchar* lower_row = src.ptr<uchar>(y2);
+        
+        // Negative cols.
+        memset(ptr_dst, upper_row[0], last_negative_col);
 
-        // (col+0.5)*scale_x-0.5 < 0
-        int last_negative_col = (int)(0.5f / scale_x - 0.5f);
-        for (int col = 0; col < last_negative_col; ++col)
-        {
-            ptr_dst[col] = (y1 == y2) ? upper_row[0] : (upper_row[0] * (y2 - y) + lower_row[0] * (y - y1));
-        }
+        // Positive cols.
         for (int col = last_negative_col; col < dst_cols; ++col)
         {
             const float x = col * scale_x + scalar_x0;
             const int x1 = (int)x;
             const int x2 = x1 + 1;
 
-            const uchar q11 = upper_row[x1];
-            const uchar q12 = lower_row[x1];
-            const uchar q21 = upper_row[x2];
-            const uchar q22 = lower_row[x2];
+            ptr_dst[col] = upper_row[x1] * (x2 - x) + upper_row[x2] * (x - x1);
+        }
+    }
 
-            ptr_dst[col] = (y1 == y2) ? (q11 * (x2 - x) + q22 * (x - x1)) : 
-              (q11 * (x2 - x) * (y2 - y) + q21 * (x - x1) * (y2 - y) + q12 * (x2 - x) * (y - y1) + q22 * (x - x1) * (y - y1));
+    // Positive rows.
+    for (int row = last_negative_row; row < dst_rows; row++)
+    {
+        uchar *ptr_dst = dst.ptr<uchar>(row);
+
+        const float y = row * scale_y + scalar_y0;
+        const int y1 = (int)y;
+        const int y2 = y1 + 1;
+        const uchar* upper_row = src.ptr<uchar>(y1);
+        const uchar* lower_row = src.ptr<uchar>(y2);
+
+        // Negative cols.
+        memset(ptr_dst,  upper_row[0] * (y2 - y) + lower_row[0] * (y - y1), last_negative_col);
+
+        // Positive cols.
+        for (int col = last_negative_col; col < dst_cols; ++col)
+        {
+            const float x = col * scale_x + scalar_x0;
+            const int x1 = (int)x;
+            const int x2 = x1 + 1;
+
+            ptr_dst[col] = upper_row[x1] * (x2 - x) * (y2 - y) + upper_row[x2] * (x - x1) * (y2 - y) +
+                           lower_row[x1] * (x2 - x) * (y - y1) + lower_row[x2] * (x - x1) * (y - y1);
         }
     }
 }
